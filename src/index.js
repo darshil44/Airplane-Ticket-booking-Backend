8a7437de5bc1c2a57d2d87ce4b454b71100c05ce
Darshil Dobariya <89260736+darshil44@users.noreply.github.com>
2025-07-31 18:38:20 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 
+const express = require('express')
+const {serverConfig, Logger} = require('./config')
+const app = express();
+
+const apiRoutes = require('./routes')
+
+app.use('/api', apiRoutes)
+
+app.listen(serverConfig.PORT, ()=>{
+    console.log(`server started sucessfully on : ${serverConfig.PORT}`); 
+    Logger.info('successfully started server', {})
+})
